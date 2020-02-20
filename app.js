f30deaf75e5b613040ebf4a6e571cb61b7360e6a
Rasmus Hebsgaard <r.hebsgaard@gmail.com>
2020-02-20 18:56:50 +0100

    init node.js

---
+++ app.js
@@ -0,0 +1,8 @@
+/* 
+ * 1. Parse command-line input
+ * 2. Validate that it is a SQL SELECT command 
+ * 3. Read data from database
+ * 4. Write data to file
+ * 5. Verify data in file
+ * 6. Delete data from database 
+ */
